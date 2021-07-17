dca56de885d661bfd2f171f29e1cd007bd6a2aeb
milianmao <1145909208@qq.com>
2021-07-17 12:00:24 +0800

    IU-2020.3.2 <"milian"@24P4WU9TOBPTUN4 Create CodeGlance.xml, ui.lnf.xml, debugger.xml, colors.scheme.xml, databaseDrivers.xml, ide-features-trainer.xml, updates.xml, keymap.xml, editor.xml, Class.java, jdk.table.xml, Default.xml, ide.general.xml, project.default.xml, baseRefactoring.xml, filetypes.xml, editor.codeinsight.xml, wsl.distributions.xml, pluginAdvertiser.xml, _@user_Darcula.icls, path.macros.xml, find.xml, laf.xml

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,8 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+* @Author ${USER}
+* @Date ${DATE} ${TIME}
+*/
+public class ${NAME} {
+}
